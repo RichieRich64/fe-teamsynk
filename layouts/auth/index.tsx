8af814eac63e89a62eebe5b3741818eaebbfd360
Richard <rico4live@gmail.com>
2026-01-01 13:03:05 +0100

    chore: comment out loading text

--- layouts/auth/index.tsx
+++ layouts/auth/index.tsx
@@ -24,7 +24,7 @@ const AuthLayoutInner = ({ children }: Props) => {
     return (
       <div className="flex items-center justify-center min-h-screen">
         <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900"></div>
-        <p className="text-sm text-gray-600 ml-2">Checking authentication...</p>
+        {/* <p className="text-sm text-gray-600 ml-2">Checking authentication...</p> */}
       </div>
     );
   }
